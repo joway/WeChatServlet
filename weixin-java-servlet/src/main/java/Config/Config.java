6c6b62aa3a5b62255548b866a9678ca66176d472
joway <joway@joway.wang>
2015-12-16 19:50:39 +0800

    init

--- weixin-java-servlet/src/main/java/Config/Config.java
+++ weixin-java-servlet/src/main/java/Config/Config.java
@@ -7,8 +7,8 @@ package Config;
 public class Config {
 
     public static final String TOKEN = "test";
-    public static final String APPID = "wxf94c096f6cdc8467";
-    public static final String APPSECRET = "eb441751944db56c6329e208b693aeef";
+    public static final String APPID = "wxe388ab08637aa809";
+    public static final String APPSECRET = "d4624c36b6795d1d99dcf0547af5443d";
     public static final String ENCODING_AESKEY = "pwIRoLymSEzMX7YOmezy4zt8t55k0DHXwP5OZNbwlLh";
 
 
